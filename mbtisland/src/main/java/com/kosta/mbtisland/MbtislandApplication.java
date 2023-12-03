c8e2400db5439ee09793ea5194f6b6dbb1eebbbb
hanhee-kim <914gksl914@naver.com>
2023-12-03 17:03:58 +0900

    test

---
+++ mbtisland/src/main/java/com/kosta/mbtisland/MbtislandApplication.java
@@ -0,0 +1,13 @@
+package com.kosta.mbtisland;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MbtislandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MbtislandApplication.class, args);
+	}
+
+}
---
+++ mbtisland/src/test/java/com/kosta/mbtisland/MbtislandApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kosta.mbtisland;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MbtislandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
